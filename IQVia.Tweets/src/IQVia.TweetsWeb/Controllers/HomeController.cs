77a0c075b32a0cdd12106fa8b971d528a007924c
DESKTOP-CBG9LIR\austi <devcode2017@gmail.com>
2018-04-11 04:00:37 -0700

    updates fixed error page

--- IQVia.Tweets/src/IQVia.TweetsWeb/Controllers/HomeController.cs
+++ IQVia.Tweets/src/IQVia.TweetsWeb/Controllers/HomeController.cs
@@ -12,9 +12,6 @@ namespace IQVia.TweetsWeb.Controllers
     {
         public IActionResult Index()
         {
-            //var model = new List<Tweet> { new Tweet { Id = "1", Stamp = "2018/04/01", Text = "HI" }};
-
-            //return View(model);
             return View();
         }
 
